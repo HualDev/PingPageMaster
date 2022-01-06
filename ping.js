66acb423ed4afda5d010aaec37261b47e19f54bc
Hualdo <hucastillo@gmail.com>
2022-01-06 12:41:13 -0500

    Final Version mobile and desktop and hover of social networks

---
+++ ping.js
@@ -0,0 +1,32 @@
+let email=''
+
+
+function hover(){
+    document.getElementById('face').style.background='hsl(223, 87%, 63%)'
+    document.getElementById('face').style.color='white'
+}
+
+function notHover(){
+    document.getElementById('face').style.background='white'
+    document.getElementById('face').style.color='hsl(223, 87%, 63%)'
+}
+
+function hoverT(){
+    document.getElementById('twitter').style.background='hsl(223, 87%, 63%)'
+    document.getElementById('twitter').style.color='white'
+}
+
+function notHoverT(){
+    document.getElementById('twitter').style.background='white'
+    document.getElementById('ttwitter').style.color='hsl(223, 87%, 63%)'
+}
+
+function hoverI(){
+    document.getElementById('insta').style.background='hsl(223, 87%, 63%)'
+    document.getElementById('insta').style.color='white'
+}
+
+function notHoverI(){
+    document.getElementById('insta').style.background='white'
+    document.getElementById('insta').style.color='hsl(223, 87%, 63%)'
+}
